9519558e0dc62437bbba1df836f4f4d4de59a51e
fasalmohammadh <fazalmohammed9367@gmail.com>
2024-02-05 18:45:35 +0530

    feat: setup react-router-dom

---
+++ src/routes/index.tsx
@@ -0,0 +1,9 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([]);
+
+function Routes() {
+  return <RouterProvider router={router} />;
+}
+
+export default Routes;
